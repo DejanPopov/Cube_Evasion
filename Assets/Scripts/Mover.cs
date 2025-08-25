70318c4296b6f0494cfb5e3826b3bb71cfb5a08c
Dejan Popov <dekipopica92@gmail.com>
2025-08-25 15:01:49 +0200

    Taking inputs from keyboard - Old way

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0f;
-    [SerializeField] float zValue = 0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Old way of taking inputs
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical");
+
         transform.Translate(xValue,yValue,zValue);
     }
 }
--- Unity_Informator.md
+++ Unity_Informator.md
@@ -1,4 +1,4 @@
 1) Edit -> Project Settings ->  When entering Play Mode -> Do not reload Domain or Scene => Will kepp windows poping when hitting PLAY button
-
+Old way of taking inputs -> Edit -> Proj. Settings -> Player -> Other SettiNgs -> Active Input Handlping -> OLD
 
 [SerializeField] Variable will show up in Unity Editor
