1640159cc10782cea698475e1eadf274e2970875
Dejan Popov <dekipopica92@gmail.com>
2025-08-26 19:55:50 +0200

    Changing object color on collision usingg GetCOmponenet<MeshRenderer>

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,7 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        GetComponent<MeshRenderer>().material.color = Color.gold;
         Debug.Log("Something hit me!");
     }
 }
