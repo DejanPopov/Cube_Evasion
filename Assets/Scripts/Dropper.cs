989851ed5b295214c7624c2af012d2b47ce75f39
Dejan Popov <dekipopica92@gmail.com>
2025-08-27 15:18:00 +0200

    Cashing Componenets and using them to make object fall from above and show its mesh

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,23 @@ public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToWait = 2f;
 
+    MeshRenderer myMyeshRenderer;
+    Rigidbody    myRigidBody;
+
     void Start()
     {
-        
+        myMyeshRenderer         = GetComponent<MeshRenderer>();
+        myRigidBody             = GetComponent<Rigidbody>();
+        myMyeshRenderer.enabled = false;
+        myRigidBody.useGravity  = false;
     }
 
     void Update()
     {
         if (Time.time > timeToWait)
         {
+            myMyeshRenderer.enabled = true;
+            myRigidBody.useGravity  = true;
             Debug.Log("Dropper is falling!");
         }
     }
