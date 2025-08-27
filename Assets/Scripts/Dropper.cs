cde68bf25a90751bfa659b46b8919faabea80944
Dejan Popov <dekipopica92@gmail.com>
2025-08-27 12:56:21 +0200

    Added script for object falling to count time when game started

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log("Time passed science game started: " + Time.time);
+    }
+}
